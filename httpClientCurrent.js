79962a1d7969281cf0e4bdcf1105120d744dae00
vaibhavfeat <133837376+vaibhavfeat@users.noreply.github.com>
2025-05-29 18:58:48 +0530

    Update httpClientCurrent.js

--- httpClientCurrent.js
+++ httpClientCurrent.js
@@ -1,10 +1,8 @@
-/*
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Licensed under the MIT License.
- */
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
 
-import http from "http";
-import https from "https";
+const http = require("http");
+const https = require("https");
 
 const HttpMethod = {
     GET: "get",
@@ -27,9 +25,6 @@ const ProxyStatus = {
     SERVER_ERROR: 500,
 };
 
-/**
- * Constants
- */
 const Constants = {
     AUTHORIZATION_PENDING: "authorization_pending",
 };
@@ -37,16 +32,12 @@ const Constants = {
 class NetworkUtils {
     static getNetworkResponse(headers, body, statusCode) {
         return {
-            headers: headers,
-            body: body,
+            headers,
+            body,
             status: statusCode,
         };
     }
 
-    /*
-     * Utility function that converts a URL object into an ordinary options object as expected by the
-     * http.request and https.request APIs.
-     */
     static urlToHttpOptions(url) {
         const options = {
             protocol: url.protocol,
@@ -69,20 +60,12 @@ class NetworkUtils {
     }
 }
 
-/**
- * This class implements the API for network requests.
- */
-export class HttpClientCurrent {
+class HttpClientCurrent {
     constructor(proxyUrl, customAgentOptions) {
         this.proxyUrl = proxyUrl || "";
         this.customAgentOptions = customAgentOptions || {};
     }
 
-    /**
-     * Http Get request
-     * @param url
-     * @param options
-     */
     async sendGetRequestAsync(url, options) {
         if (this.proxyUrl) {
             return networkRequestViaProxy(
@@ -102,11 +85,6 @@ export class HttpClientCurrent {
         }
     }
 
-    /**
-     * Http Post request
-     * @param url
-     * @param options
-     */
     async sendPostRequestAsync(url, options, cancellationToken) {
         if (this.proxyUrl) {
             return networkRequestViaProxy(
@@ -122,19 +100,14 @@ export class HttpClientCurrent {
                 url,
                 HttpMethod.POST,
                 options,
-                this.customAgentOptions
+                this.customAgentOptions,
+                cancellationToken
             );
         }
     }
-	
-	const networkRequestViaProxy = (
-    destinationUrlString,
-    proxyUrlString,
-    httpMethod,
-    options,
-    agentOptions,
-    timeout
-) => {
+}
+
+function networkRequestViaProxy(destinationUrlString, proxyUrlString, httpMethod, options, agentOptions, timeout) {
     const destinationUrl = new URL(destinationUrlString);
     const proxyUrl = new URL(proxyUrlString);
 
@@ -144,7 +117,7 @@ export class HttpClientCurrent {
         port: proxyUrl.port,
         method: "CONNECT",
         path: destinationUrl.hostname,
-        headers: headers,
+        headers,
     };
 
     if (timeout) {
@@ -156,19 +129,18 @@ export class HttpClientCurrent {
     }
 
     let postRequestStringContent = "";
-    if (httpMethod === "POST") {
+    if (httpMethod === HttpMethod.POST) {
         const body = options?.body || "";
         postRequestStringContent =
             "Content-Type: application/x-www-form-urlencoded\r\n" +
-            `Content-Length: ${body.length}\r\n` +
-            `\r\n${body}`;
+            `Content-Length: ${body.length}\r\n\r\n${body}`;
     }
+
     const outgoingRequestString =
         `${httpMethod.toUpperCase()} ${destinationUrl.href} HTTP/1.1\r\n` +
         `Host: ${destinationUrl.host}\r\n` +
         "Connection: close\r\n" +
-        postRequestStringContent +
-        "\r\n";
+        postRequestStringContent + "\r\n";
 
     return new Promise((resolve, reject) => {
         const request = http.request(tunnelRequestOptions);
@@ -183,20 +155,16 @@ export class HttpClientCurrent {
         request.end();
 
         request.on("connect", (response, socket) => {
-            const proxyStatusCode =
-                response?.statusCode || 500; // Assuming 500 as ProxyStatus.SERVER_ERROR
+            const proxyStatusCode = response?.statusCode || ProxyStatus.SERVER_ERROR;
             if (
-                proxyStatusCode < 200 ||
-                proxyStatusCode > 299
+                proxyStatusCode < ProxyStatus.SUCCESS_RANGE_START ||
+                proxyStatusCode > ProxyStatus.SUCCESS_RANGE_END
             ) {
                 request.destroy();
                 socket.destroy();
-                reject(
-                    new Error(
-                        `Error connecting to proxy. Http status code: ${response.statusCode}. Http status message: ${response?.statusMessage || "Unknown"}`
-                    )
-                );
+                return reject(new Error(`Error connecting to proxy. Http status code: ${response.statusCode}. Http status message: ${response?.statusMessage || "Unknown"}`));
             }
+
             if (tunnelRequestOptions.timeout) {
                 socket.setTimeout(tunnelRequestOptions.timeout);
                 socket.on("timeout", () => {
@@ -209,22 +177,162 @@ export class HttpClientCurrent {
             socket.write(outgoingRequestString);
 
             const data = [];
-            socket.on("data", (chunk) => {
-                data.push(chunk);
-            });
-
+            socket.on("data", (chunk) => data.push(chunk));
             socket.on("end", () => {
                 const dataString = Buffer.concat(data).toString();
                 const dataStringArray = dataString.split("\r\n");
-                const httpStatusCode = parseInt(
-                    dataStringArray[0].split(" ")[1]
+                const httpStatusCode = parseInt(dataStringArray[0].split(" ")[1]);
+                const statusMessage = dataStringArray[0].split(" ").slice(2).join(" ");
+                const body = dataStringArray[dataStringArray.length - 1];
+                const headersArray = dataStringArray.slice(1, dataStringArray.length - 2);
+
+                const entries = new Map();
+                headersArray.forEach((header) => {
+                    const headerKeyValue = header.split(new RegExp(/:\s(.*)/s));
+                    const headerKey = headerKeyValue[0];
+                    let headerValue = headerKeyValue[1];
+                    try {
+                        const object = JSON.parse(headerValue);
+                        if (object && typeof object === "object") {
+                            headerValue = object;
+                        }
+                    } catch (e) { }
+                    entries.set(headerKey, headerValue);
+                });
+
+                const headers = Object.fromEntries(entries);
+                const networkResponse = NetworkUtils.getNetworkResponse(
+                    headers,
+                    parseBody(httpStatusCode, statusMessage, headers, body),
+                    httpStatusCode
                 );
-                // Further processing can be done here
+
+                if (
+                    (httpStatusCode < HttpStatus.SUCCESS_RANGE_START || httpStatusCode > HttpStatus.SUCCESS_RANGE_END) &&
+                    networkResponse.body["error"] !== Constants.AUTHORIZATION_PENDING
+                ) {
+                    request.destroy();
+                }
+
+                resolve(networkResponse);
+            });
+
+            socket.on("error", (chunk) => {
+                request.destroy();
+                socket.destroy();
+                reject(new Error(chunk.toString()));
             });
         });
+
+        request.on("error", (chunk) => {
+            request.destroy();
+            reject(new Error(chunk.toString()));
+        });
     });
-};
-
-
 }
 
+function networkRequestViaHttps(urlString, httpMethod, options, agentOptions, timeout) {
+    const isPostRequest = httpMethod === HttpMethod.POST;
+    const body = options?.body || "";
+
+    const url = new URL(urlString);
+    const headers = options?.headers || {};
+    let customOptions = {
+        method: httpMethod,
+        headers,
+        ...NetworkUtils.urlToHttpOptions(url),
+    };
+
+    if (timeout) {
+        customOptions.timeout = timeout;
+    }
+
+    if (agentOptions && Object.keys(agentOptions).length) {
+        customOptions.agent = new https.Agent(agentOptions);
+    }
+
+    if (isPostRequest) {
+        customOptions.headers = {
+            ...customOptions.headers,
+            "Content-Length": body.length,
+        };
+    }
+
+    return new Promise((resolve, reject) => {
+        const request = https.request(customOptions);
+
+        if (timeout) {
+            request.on("timeout", () => {
+                request.destroy();
+                reject(new Error("Request time out"));
+            });
+        }
+
+        if (isPostRequest) {
+            request.write(body);
+        }
+
+        request.end();
+
+        request.on("response", (response) => {
+            const headers = response.headers;
+            const statusCode = response.statusCode;
+            const statusMessage = response.statusMessage;
+
+            const data = [];
+            response.on("data", (chunk) => data.push(chunk));
+            response.on("end", () => {
+                const body = Buffer.concat(data).toString();
+
+                const networkResponse = NetworkUtils.getNetworkResponse(
+                    headers,
+                    parseBody(statusCode, statusMessage, headers, body),
+                    statusCode
+                );
+
+                if (
+                    (statusCode < HttpStatus.SUCCESS_RANGE_START || statusCode > HttpStatus.SUCCESS_RANGE_END) &&
+                    networkResponse.body["error"] !== Constants.AUTHORIZATION_PENDING
+                ) {
+                    request.destroy();
+                }
+
+                resolve(networkResponse);
+            });
+        });
+
+        request.on("error", (chunk) => {
+            request.destroy();
+            reject(new Error(chunk.toString()));
+        });
+    });
+}
+
+function parseBody(statusCode, statusMessage, headers, body) {
+    let parsedBody;
+    try {
+        parsedBody = JSON.parse(body);
+    } catch (error) {
+        let errorType;
+        let errorDescriptionHelper;
+        if (statusCode >= HttpStatus.CLIENT_ERROR_RANGE_START && statusCode <= HttpStatus.CLIENT_ERROR_RANGE_END) {
+            errorType = "client_error";
+            errorDescriptionHelper = "A client";
+        } else if (statusCode >= HttpStatus.SERVER_ERROR_RANGE_START && statusCode <= HttpStatus.SERVER_ERROR_RANGE_END) {
+            errorType = "server_error";
+            errorDescriptionHelper = "A server";
+        } else {
+            errorType = "unknown_error";
+            errorDescriptionHelper = "An unknown";
+        }
+
+        parsedBody = {
+            error: errorType,
+            error_description: `${errorDescriptionHelper} error occured.\nHttp status code: ${statusCode}\nHttp status message: ${statusMessage || "Unknown"}\nHeaders: ${JSON.stringify(headers)}`
+        };
+    }
+
+    return parsedBody;
+}
+
+module.exports = { HttpClientCurrent };
